16c2e8ce97f2663cfc7dbe248ef9ee05753a3518
Daniel <dcerralbo@gmail.com>
2019-03-11 17:02:55 +0100

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,9 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Daniel:LSX8vbHmQF8QXbV@cluster0-rjtn1.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
-
-
 
 process.env.URLDB = urlDB;
